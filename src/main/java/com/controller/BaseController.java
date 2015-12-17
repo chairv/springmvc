fd481daeee13bf7ae429aff648988d250a284ecc
chairv <changwei114@126.com>
2015-12-18 00:00:32 +0800

    抖动

--- src/main/java/com/controller/BaseController.java
+++ src/main/java/com/controller/BaseController.java
@@ -1,10 +1,5 @@
 package com.controller;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by qiao on 2015/11/18.
