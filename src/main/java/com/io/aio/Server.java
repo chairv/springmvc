7872b18bd8e252f27d116b9a808ef77545f7c639
tancw <changwei114@126.com>
2017-06-13 16:22:32 +0800

    aio

---
+++ src/main/java/com/io/aio/Server.java
@@ -0,0 +1,55 @@
+package com.io.aio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by tancw on 2017/6/13.
+ */
+public class Server {
+    private static Charset charset = Charset.forName("US-ASCII");
+    private static CharsetEncoder encoder = charset.newEncoder();
+
+    public static void main(String[] args) {
+        try {
+            AsynchronousChannelGroup group = AsynchronousChannelGroup.withThreadPool(Executors.newFixedThreadPool(4));
+            AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open(group).bind(new InetSocketAddress("0.0.0.0", 8013));
+            server.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
+                @Override
+                public void completed(AsynchronousSocketChannel result, Void attachment) {
+                    server.accept(null, this); //接受下一个连接
+                    try {
+                        String now = new Date().toString();
+                        ByteBuffer buffer = encoder.encode(CharBuffer.wrap(now + "\r\n"));
+                        Future<Integer> f = result.write(buffer);
+                        f.get();
+                        System.out.println("send to client: " + now);
+                        result.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void failed(Throwable exc, Void attachment) {
+                    exc.printStackTrace();
+                }
+            });
+            group.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
