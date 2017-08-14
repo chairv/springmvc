96ab4f55e9d0e407ef281d895a0825b3898e55b0
tancw <changwei114@126.com>
2017-08-14 18:52:51 +0800

    menu

--- src/test/java/NumberTest.java
+++ src/test/java/NumberTest.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 import org.junit.Test;
 
@@ -10,5 +11,16 @@ public class NumberTest {
     @Test public  void test(){
         BigDecimal b= new BigDecimal("20.12312312");
         System.out.println(b.setScale(2));
+    }
+
+    @Test
+    public void testFormat(){
+        NumberFormat format = NumberFormat.getPercentInstance();
+        format.setMinimumFractionDigits(2);
+//        System.out.println(format.format(17.2/100.0));
+
+
+        BigDecimal number  = new BigDecimal("0.16");
+        System.out.println(format.format(number.doubleValue()));
     }
 }
