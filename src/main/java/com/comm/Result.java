47e0853ae48860ef3c3a1563b36c401762aca22b
chairv <changwei114@126.com>
2018-01-05 11:00:30 +0800

    test format

--- src/main/java/com/comm/Result.java
+++ src/main/java/com/comm/Result.java
@@ -1,30 +1,43 @@
 package com.comm;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
+
 /**
  * Created by tancw on 2015/11/26.
  */
 public class Result {
-	private boolean success;
-	private String message;
+    private boolean success;
+    private String message;
 
-	public Result(boolean success, String message) {
-		this.success = success;
-		this.message = message;
-	}
+    public Result(){}
 
-	public boolean isSuccess() {
-		return success;
-	}
+    public Result(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
 
-	public void setSuccess(boolean success) {
-		this.success = success;
-	}
+    public boolean isSuccess() {
+        return success;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        Class c = Result.class;
+        Object obj = c.newInstance();
+        BeanUtils.setProperty(obj,"message","1");
+    }
 }
--- src/test/java/NumberTest.java
+++ src/test/java/NumberTest.java
@@ -1,6 +1,8 @@
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 /**
@@ -8,19 +10,35 @@ import org.junit.Test;
  */
 public class NumberTest {
 
-    @Test public  void test(){
-        BigDecimal b= new BigDecimal("20.12312312");
+    @Test
+    public void test() {
+        BigDecimal b = new BigDecimal("20.12312312");
         System.out.println(b.setScale(2));
     }
 
     @Test
-    public void testFormat(){
+    public void testFormat() {
         NumberFormat format = NumberFormat.getPercentInstance();
         format.setMinimumFractionDigits(2);
 //        System.out.println(format.format(17.2/100.0));
 
 
-        BigDecimal number  = new BigDecimal("0.16");
+        BigDecimal number = new BigDecimal("0.16");
         System.out.println(format.format(number.doubleValue()));
     }
+
+    @Test
+    public void bigInt() {
+        BigDecimal bigDecimal = new BigDecimal("1.5");
+        System.out.println(bigDecimal.toPlainString());
+        System.out.println(StringUtils.capitalize("name"));
+    }
+
+    @Test
+    public void bigFormat() {
+        DecimalFormat percentageFormat = new DecimalFormat("0.##%");
+        BigDecimal big = new BigDecimal("0.1010");
+        System.out.println(percentageFormat.format(big));
+    }
+
 }
