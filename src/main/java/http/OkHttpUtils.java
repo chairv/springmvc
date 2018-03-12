fd5b954fe51caac95f621bf0377511b3bc151f66
chairv <changwei114@126.com>
2018-03-12 15:47:18 +0800

    menu

--- src/main/java/http/OkHttpUtils.java
+++ src/main/java/http/OkHttpUtils.java
@@ -1,8 +1,10 @@
 package http;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -12,15 +14,37 @@ import okhttp3.Response;
  * Created by tancw on 2017/7/19.
  */
 public class OkHttpUtils {
-    final static OkHttpClient  cLient = new OkHttpClient();
-    public static void main(String[] args) {
-        String url = "http://localhost:8123/refresh";
-        RequestBody body = new FormBody.Builder().build();
-        Request request = new Request.Builder().url(url).post(body).build();
-        try {
-            Response response = cLient.newCall(request).execute();
-        } catch (IOException e) {
-            e.printStackTrace();
+    final static OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build();
+
+//    public static void main(String[] args) {
+//        String url = "http://localhost:8123/refresh";
+//        RequestBody body = new FormBody.Builder().build();
+//        Request request = new Request.Builder().url(url).post(body).build();
+//        try {
+//            Response response = cLient.newCall(request).execute();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    public static String post(String url, String jsonParam) throws IOException {
+        RequestBody requestBody = FormBody.create(MediaType.parse("application/json; charset=utf-8")
+                , jsonParam);
+        Request request = new Request.Builder()
+                .url(url)//请求的url
+                .post(requestBody)
+                .build();
+        Response response = okHttpClient.newCall(request).execute();
+        if (response.isSuccessful()) {
+            return response.body().string();
+        } else {
+            throw new IOException("Unexpected code " + response);
         }
     }
+
+
 }
--- src/main/java/wx/menu/MenuTest.java
+++ src/main/java/wx/menu/MenuTest.java
@@ -1,17 +1,8 @@
 package wx.menu;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.HttpClientConnectionManager;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.util.EntityUtils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import http.OkHttpUtils;
 
 /**
  * Created by tancw on 2017/7/25.
@@ -21,33 +12,24 @@ public class MenuTest {
     public static final String ACCESS_TOKEN = "7n1CsA0cBwOBmGNc3TGezhpuGC7GWG0QrSCxDxG5c2z1Faz7cviNKEUuAaPmkIF9eMRJy4GER6olvDAWKacpYBR7AcJU4jt0Zjy_Rp-DGwDHrM2goT1b3sbkXUX38btvFBWiCHASOJ";
 
     public static void main(String[] args) throws Exception {
-        String str = IOUtils.toString(Thread.currentThread().getContextClassLoader().getResource("menu.json"));
-        String menuStr = parse(str);
-        String mode = SysConf.get("env.mode");
-        if (StringUtils.equals("develop", mode)) {
-            String res = createMenu(menuStr, ACCESS_TOKEN);
-            System.out.println(res);
-        }
+        String menuStr = IOUtils.toString(Thread.currentThread().getContextClassLoader().getResource("menu.json"));
+        String res = createMenu(menuStr, ACCESS_TOKEN);
+        System.out.println(res);
     }
 
     public static String createMenu(String params, String accessToken) throws Exception {
-        HttpPost httpost = HttpClientConnectionManager.getPostMethod("https://api.weixin.qq.com/cgi-bin/menu/create?access_token=" + accessToken);
-        httpost.setEntity(new StringEntity(params, "UTF-8"));
-        HttpResponse response = httpclient.execute(httpost);
-        String jsonStr = EntityUtils.toString(response.getEntity(), "utf-8");
-        System.out.println(jsonStr);
-        JSONObject object = JSON.parseObject(jsonStr);
-        return object.getString("errmsg");
+        String url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=" + accessToken;
+        return OkHttpUtils.post(url, params);
     }
 
-    private static String parse(String str) {
-        Pattern pattern = Pattern.compile(DEF_REGEX);
-        Matcher matcher = pattern.matcher(str);
-        while (matcher.find()) {
-            String name = matcher.group(1);// 键名
-            String realVal = SysConf.get(name);
-            str = StringUtils.replace(str, matcher.group(), realVal);
-        }
-        return str;
-    }
+//    private static String parse(String str) {
+//        Pattern pattern = Pattern.compile(DEF_REGEX);
+//        Matcher matcher = pattern.matcher(str);
+//        while (matcher.find()) {
+//            String name = matcher.group(1);// 键名
+//            String realVal = SysConf.get(name);
+//            str = StringUtils.replace(str, matcher.group(), realVal);
+//        }
+//        return str;
+//    }
 }
